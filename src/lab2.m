f64e10bb4e67b710f3ef43952bb43a6d54075bdf
mike laks <mjlaks@wpi.edu>
2020-01-29 17:11:32 -0500

    Added a lab2 script to calibrate

---
+++ src/lab2.m
@@ -0,0 +1,56 @@
+clear
+clear java
+clear classes;
+
+addpath('../lib'); % Ensure that lib folder is on filepath
+
+vid = hex2dec('3742');
+pid = hex2dec('0007');
+
+disp (vid);
+disp (pid);
+
+javaaddpath ../lib/SimplePacketComsJavaFat-0.6.4.jar;
+import edu.wpi.SimplePacketComs.*;
+import edu.wpi.SimplePacketComs.device.*;
+import edu.wpi.SimplePacketComs.phy.*;
+import java.util.*;
+import org.hid4java.*;
+version -java
+myHIDSimplePacketComs=HIDfactory.get();
+myHIDSimplePacketComs.setPid(pid);
+myHIDSimplePacketComs.setVid(vid);
+myHIDSimplePacketComs.connect();
+
+% Create a PacketProcessor object to send data to the nucleo firmware
+pp = PacketProcessor(myHIDSimplePacketComs);
+
+%% Run status command 6 times and record data in a .csv file with the timestamp as a name
+csvfile = fopen(sprintf('../logs/log_%s.csv', datestr(now, 'mm-dd-yyyy_HH-MM-SS')), 'a');
+fprintf(csvfile, 'Encoder_Joint1,Encoder_Joint2,Encoder_Joint3,Velocity_Joint1,Velocity_Joint2,Velocity_Joint3,\n');
+for k=1:11 %% Revise maximum to number of datapoints to be recorded
+    returnPacket=status(pp);
+    fprintf(csvfile, '%f,%f,%f,%f,%f,%f,\n', returnPacket(1:11));
+    pause(.5);
+end
+fclose(csvfile);
+
+%% Send calibration packet
+statusPacket = status(pp);
+statusPacket = status(pp);
+statusPacket = status(pp);
+statusPacket = status(pp);
+statusPacket = status(pp);
+statusPacket = status(pp);
+statusPacket = status(pp);
+statusPacket = status(pp);
+statusPacket = status(pp);
+statusPacket = status(pp);
+statusPacket = status(pp);
+
+%Encoder1: 9.7666666667	Encoder2: 18.8125	Encoder3: -10.2064393939
+
+
+% Clear up memory upon termination
+pp.shutdown()
+
