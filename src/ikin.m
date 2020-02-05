f0ed8e1845e2edb5623d60476a3069700ba69c8a
Teresa Saddler <tsaddler@wpi.edu>
2020-02-05 16:15:38 -0500

    Fix calculation errors in inverse kinematics script

--- src/ikin.m
+++ src/ikin.m
@@ -3,7 +3,8 @@ function [theta1, theta2, theta3] = ikin(x, y, z)
 
     r = sqrt(x^2 + y^2);
     s = z - L(1);
-    theta1 = pi + atan2(y,x);
+    %theta1 = pi + atan2(y,x);
     theta3 = (L(2)^2 + L(3)^2 - (r^2 + z^2))/(2*L(2)*L(3));
-    theta2 = asin((s-L(3)*sin(theta3)/r));
+    theta2 = atan((s - L(3) * sin(theta3)) / r);
+    theta1 = atan2(y,x);
 end
