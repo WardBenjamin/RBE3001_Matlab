57beb27baa1dca77ab19815eca7aaafd3331d66b
Benjamin Ward <blward@wpi.edu>
2020-03-01 17:43:50 -0500

    Fix crash; better size detection code

--- src/util/findObjSizes.m
+++ src/util/findObjSizes.m
@@ -36,6 +36,7 @@ function [yellowObjects, greenObjects, blueObjects, blackObjects] = findObjSizes
 
 yLen = 0;
 gLen = 0;
+bLen = 0;
 
 if ~isempty(yC)
     yLen = length(yC(1:end, 1))
@@ -43,17 +44,22 @@ end
 if ~isempty(gC)
     gLen = length(gC(1:end, 1))
 end
+if ~isempty(bC)
+    bLen = length(bC(1:end, 1))
+end
+
+yellowObjects = [];
+greenObjects = [];
+blueObjects = [];
 
 if yLen == 0
-    yellowObjects = [];
-    
     if gLen ~= 0
         greenObjects = [gC objSize(1:gLen, 1)];
-    else
-        greenObjects = [];
     end
     
-    blueObjects = [bC objSize(gLen + 1:end, 1)];
+    if bLen ~= 0
+        blueObjects = [bC objSize(gLen + 1:end, 1)];
+    end
 else
     yellowObjects = [yC objSize(1:yLen, 1)];
     
@@ -61,11 +67,11 @@ else
     
     if gLen ~= 0
         greenObjects = [gC objSize(yLen + 1:gEnd, 1)];
-    else
-        greenObjects = [];
     end
     
-    blueObjects = [bC objSize(gEnd + 1:end, 1)];
+    if bLen ~= 0
+        blueObjects = [bC objSize(gEnd + 1:end, 1)];
+    end
 end
 
 % yellowObjects = [yC objSize(1:yEnd, 1)]
@@ -95,11 +101,11 @@ function [objSize, foundObjects] = findObjectSize(coloredCentroids, kMask)
     foundObjects = zeros(length(nearestIdx), 3);
     objSize = zeros(length(nearestIdx), 1);
     for idx = 1:length(nearestIdx)
-        % diaMean = mean([stats.MajorAxisLength(nearestIdx(idx)),
-        % stats.MinorAxisLength(nearestIdx(idx))],2);
+%         objDia = max([stats.MajorAxisLength(nearestIdx(idx)), stats.MinorAxisLength(nearestIdx(idx))]);
         objDia = stats.MajorAxisLength(nearestIdx(idx));
+        objMinorDia = stats.MinorAxisLength(nearestIdx(idx));
         foundObjects(idx, :) = [kCentroids(nearestIdx(idx), :) objDia/2];
-        objSize(idx) = objDia > 100;
+        objSize(idx) = (objDia > 100) & (objMinorDia > 60); % Prev: 100; didn't work for 
     end
     
     
